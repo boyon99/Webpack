f6b3ed636ec8d2c61ffd2c55dbc32aeb4ee1a81a
boyon99 <bo_yon_99@naver.com>
2023-02-03 14:39:42 +0900

    :wrench: conf: html-webpack-plugin
    npm run dev을 통해 html파일을 열 수 있다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
-const path = require('path')
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   // 파일을 읽기 위한 진입점 설정
-  entry:'./js/main.js',
+  entry: './js/main.js',
 
   // 번들을 반환하는 설정
   output: {
-    path: path.resolve(__dirname,'dist'),
-    filename:'main.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+
+  // enrty에서 output하는 과정에서 plugin을 사용한다.
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html' // 템플릿으로 해당 파일을 사용한다.
+    })
+  ],
+
+  devServer: {
+    host: 'localhost'
   }
 }
